7f1e12bb4875440cfe85b99f599f1257ffdecc17
stu-pksp120 <stu-pksp120@hq.corp.vlsu.ru>
2022-12-13 08:59:01 +0300

    Добавьте файлы проекта.

---
+++ Task8Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplication1
+{
+    class xeks
+    {
+        public void m()
+        {
+            Console.WriteLine("введиет дату в формате дата.месяц.год(пример 30.01.2016)");
+            string text = Convert.ToString(Console.ReadLine());
+
+            string pattern = "[0-3][0-9].[0-1][0-9].[1,2][9,0][0-9][0-9]";
+
+            MatchCollection matches;
+            Regex reg = new Regex(pattern);
+            matches = reg.Matches(text);
+
+            {
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string updDate = DateTime.Parse(matches[i].Value).AddDays(+1).ToShortDateString();
+                    text = text.Replace(matches[i].Value, updDate);
+                }
+                Console.WriteLine("Дата следующего дня дня {0}", text);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            xeks f1 = new xeks();
+
+            f1.m();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Task8Form/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace Task8Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            string pattern = "[0-3][0-9].[0-1][0-9].[1,2][9,0][0-9][0-9]";
+
+            MatchCollection matches;
+            Regex reg = new Regex(pattern);
+            matches = reg.Matches(text);
+
+            {
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string updDate = DateTime.Parse(matches[i].Value).AddDays(+1).ToShortDateString();
+                    text = text.Replace(matches[i].Value, updDate);
+                }
+                textBox2.Text += text;
+            }
+        }
+    }
+}
